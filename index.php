fa7cd6cd36e8cc3924abf27861808ab7bf27ae8b
valentinenhapngu <valentine.nhapngu@gmail.com>
2021-10-29 15:55:42 +0700

    mvc demo 1 up

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
 <?php
-echo "hello world!<br>".PHP_EOL;
-echo "test webhook, hihi<br>".PHP_EOL;
-echo "intall code, clone git<br>".PHP_EOL;
-
-include "core/db/sqlite.php";
-
-$x = new SQLiteConnection;
-$x->connect();
-
-// *** INSERT INTO abouts(id,name,des,scr,note) VALUES(?,?,?,?,?) (mang)
-// $tbname = "abouts";
-// $arr_value = [
-//     "field_selected" => ['id','name','des','scr','note'],
-//     "field_value_list" => [
-//         [ 3 , "name3", "name3", "name3", "name3"],
-//         [ 4 , "name3", "name3", "name3", "name3" ]
-//     ]
-// ];
-// $x->insert($tbname,$arr_value["field_selected"],$arr_value["field_value_list"]);
-
-
-// xxx - tesstt git after clon
-
-// tesst on Hackintosh v2
-
-$x->disconect();
-
+session_start();
+require_once "./mvc/Bridge.php";
+$myApp = new App();
 ?>
