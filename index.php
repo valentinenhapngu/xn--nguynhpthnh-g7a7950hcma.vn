21655951c16c6aae3fbab8655705e0c7fe32921e
valentinenhapngu <valentine.nhapngu@gmail.com>
2021-10-29 16:23:32 +0700

    connect db

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
-session_start();
-require_once "./mvc/Bridge.php";
-$myApp = new App();
+// session_start();
+// require_once "./mvc/Bridge.php";
+// $myApp = new App();
+
+// class DB{
+
+//     public $con;
+//     protected $servername = "localhost";
+//     protected $username = " u848302103_root";
+//     protected $password = "1234@Abcd@";
+//     protected $dbname = "u848302103_php_mvc";
+
+//     function __construct(){
+//         $this->con = mysqli_connect($this->servername, $this->username, $this->password);
+//         mysqli_select_db($this->con, $this->dbname);
+//         mysqli_query($this->con, "SET NAMES 'utf8'");
+//     }
+
+// } 
+include_once "./mvc/core/DB.php";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+
+
 ?>
